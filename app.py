fc0fed20b76d063860ddea51572205861f2c1823
alissonsenaadm <alissonsenaadm@gmail.com>
2026-09-06 15:33:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/", response_class=HTMLResponse)
+
+@app.get("/")
 async def pagina_inicial(request: Request):
     return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+        request,
+        "index.html"
     )
